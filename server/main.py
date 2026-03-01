b4753e4b86d771ac3fb0d26805b64b0b540359af
adithyanmarikkal <marikkaladithyan@gmail.com>
2026-03-01 10:42:27 +0530

    access given to vecel orgin

--- server/main.py
+++ server/main.py
@@ -33,7 +33,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173" , "https://your-frontend.vercel.app"] ,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
